ec3ec8443700c8c72a8b3d242db19621d7a79bed
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-05-27 13:31:56 -0500

    Probando obtener la lista de las tarjetas de una lista

--- app.js
+++ app.js
@@ -3,3 +3,23 @@ require('dotenv').config()
 if(!process.env.TOKEN && !process.env.KEY){
   throw new Error('No hay configuración con Api Key y con Token')
 }
+
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+let cardTitle = `Card Nueva ${new Date()}`
+
+let Respuesta = [];
+
+trello.getCardsOnList("629010d723c8458f52ef649f")
+.then(function(response){
+    console.log('response ', response);
+    Respuesta = response;
+}).catch(function(error){
+    console.log('error ', error);
+});
+
+console.log(Respuesta.length);
+
+
+
