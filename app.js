c7837ebbcb45eb665ddc472f4190fb04211f0a7e
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-05-27 19:14:53 -0500

    Usando la API de Trello para leer las tarjetas de una lista y borrar la última

--- app.js
+++ app.js
@@ -3,23 +3,24 @@ require('dotenv').config()
 if(!process.env.TOKEN && !process.env.KEY){
   throw new Error('No hay configuración con Api Key y con Token')
 }
-
 let Trello = require("trello");
 let trello = new Trello(process.env.KEY, process.env.TOKEN);
 
-let cardTitle = `Card Nueva ${new Date()}`
+let Respuesta = trello.getCardsOnList("629010d723c8458f52ef649f");
+Respuesta.then((cards) => {
+    let Ids = cards.map((card) => {
+        return card.id;
+    });
+    
+    trello.deleteCard(Ids[Ids.length - 1], 
+        function(error){
+            if (error){
+                console.log('Could not delete card:', error);
+            }else{
+                console.log("Card deleted successful");
+            }
+        }
+    )
 
-let Respuesta = [];
 
-trello.getCardsOnList("629010d723c8458f52ef649f")
-.then(function(response){
-    console.log('response ', response);
-    Respuesta = response;
-}).catch(function(error){
-    console.log('error ', error);
 });
-
-console.log(Respuesta.length);
-
-
-
